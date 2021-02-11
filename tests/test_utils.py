9fe3cb754f79c8481638aaf4800ffb7080211114
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-02-11 23:11:34 +0100

    Add test to check Travis run

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,27 @@ Utilities Tests
 """
 
 from kwx import utils
+
+
+def test__combine_tokens_to_str():
+    texts = ["words", "to", "not", "be", "combined"]
+    texts_lol = [["words", "to"], ["not"], ["be", "combined"]]
+    ignore_words = ["not"]
+    result_ignore_not = "words to be combined"
+    result_not = "words to not be combined"
+
+    assert (
+        utils._combine_tokens_to_str(texts=texts, ignore_words=ignore_words)
+        == result_ignore_not
+    )
+
+    assert (
+        utils._combine_tokens_to_str(texts=texts_lol, ignore_words=ignore_words)
+        == result_ignore_not
+    )
+
+    assert utils._combine_tokens_to_str(texts=texts, ignore_words=None) == result_not
+
+    assert (
+        utils._combine_tokens_to_str(texts=texts_lol, ignore_words=None) == result_not
+    )
