4c3b4b18b1e523ea3fba8761d930b70159dae3ef
Andrew Tavis <andrew.t.mcallister@gmail.com>
2021-03-05 09:17:22 +0100

    Version up for LDA multicore and desciption edit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2020-2021, kwx developers (BSD License)"
 author = "kwx developers"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.2"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
