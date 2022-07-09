aee7a7fa0a3adb50b8ed1701b13fa3478c6a42dd
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2022-07-09 11:27:56 +0200

    Fix copyright date to single year in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "kwx"
-copyright = "2020-2021, kwx developers (BSD License)"
+copyright = "2020, kwx developers (BSD License)"
 author = "kwx developers"
 
 # The full version, including alpha/beta/rc tags
