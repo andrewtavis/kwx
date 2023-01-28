899fb1a842eb003b771356d53c27dd241fd6bfa2
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2023-01-28 19:06:07 +0100

    #49 update emoji to v2.x

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup_args = dict(
     name="kwx",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    version="1.0.1",
+    version="1.0.2",
     author="Andrew Tavis McAllister",
     author_email="andrew.t.mcallister@gmail.com",
     classifiers=[
